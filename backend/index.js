41f25c48a3490aa47c124a6ac7a9462b327f17fe
AnandMaharana2004 <anandmaharana427@gmail.com>
2025-03-31 14:26:11 +0530

    change ther origin in cors

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-    // origin: `http://localhost:3000`,
-    origin: `http://localhost:5173`,
+    origin: `http://localhost:3000`,
+    // origin: `http://localhost:5173`,
     credentials: true
 }))
 
